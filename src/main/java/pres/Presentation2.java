8ea7f2f64fa35b4135ebe18b369368697d1a6867
MCwalid <benazzouzwalid82@gmail.com>
2025-04-12 12:04:27 +0100

    troisieme commit

--- src/main/java/pres/Presentation2.java
+++ src/main/java/pres/Presentation2.java
@@ -4,22 +4,35 @@ import dao.IDao;
 import metier.IMetier;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Presentation2 {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(new File("src/main/config.txt"));
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("src/main/config.txt"));
 
-        String daoClassName = scanner.nextLine();
-        Class cDao = Class.forName(daoClassName);
-        IDao dao = (IDao) cDao.newInstance();
+            // Lecture du nom de la classe DAO
+            String daoClassName = scanner.nextLine();
+            Class cDao = Class.forName(daoClassName);
+            IDao dao = (IDao) cDao.getConstructor().newInstance(); // Utilisation de getConstructor pour instancier correctement
 
-        String metierClassName = scanner.nextLine();
-        Class cMetier = Class.forName(metierClassName);
-        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+            // Lecture du nom de la classe Metier
+            String metierClassName = scanner.nextLine();
+            Class cMetier = Class.forName(metierClassName);
+            IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
 
-        System.out.println("RES=" + metier.calcul());
+            // Injection de l'IDao dans l'objet Metier
+            Method setDaoMethod = cMetier.getMethod("setDao", IDao.class);
+            setDaoMethod.invoke(metier, dao); // Invocation de setDao sur l'objet metier
 
-        scanner.close();
+            // Appel de la méthode calcul() et affichage du résultat
+            System.out.println("RES=" + metier.calcul());
+
+            scanner.close(); // Fermeture du scanner après utilisation
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
